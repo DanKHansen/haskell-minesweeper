8eedbd8330130732d3c6555a3298a72a6c1c08a1
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-16 13:00:38 +0200

    refactor

--- src/Minesweeper.hs
+++ src/Minesweeper.hs
@@ -1,25 +1,15 @@
 module Minesweeper (annotate) where
 
 annotate :: [String] -> [String]
-annotate l =
+annotate board =
   [ [ if c == '*'
         then '*'
         else
-          let n =
-                length
-                  [ ()
-                    | dx <- [-1 .. 1],
-                      let x = r + dx,
-                      dy <- [-1 .. 1],
-                      let y = i + dy,
-                      x /= r || y /= i,
-                      inBounds x y,
-                      (l !! x) !! y == '*'
-                  ]
+          let n = length [() | dx <- [-1 .. 1], let x = r + dx, dy <- [-1 .. 1], let y = i + dy, x /= r || y /= i, inBounds x y, (board !! x) !! y == '*']
            in if n == 0 then ' ' else head (show n)
       | (i, c) <- zip [0 ..] row
     ]
-    | (r, row) <- zip [0 ..] l
+    | (r, row) <- zip [0 ..] board
   ]
   where
-    inBounds x y = x >= 0 && x < length l && y >= 0 && y < length (head l)
+    inBounds x y = x >= 0 && x < length board && y >= 0 && y < length (head board)
