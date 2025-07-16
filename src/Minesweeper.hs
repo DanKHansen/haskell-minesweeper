1d70c9419bae6313092baf0ef7a05e7d96224851
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-16 12:32:30 +0200

    solution

--- src/Minesweeper.hs
+++ src/Minesweeper.hs
@@ -1,4 +1,26 @@
 module Minesweeper (annotate) where
 
 annotate :: [String] -> [String]
-annotate board = error "You need to implement this function."
+annotate l =
+  [ [ if (l !! r) !! c == '*'
+        then '*'
+        else
+          let n =
+                length
+                  [ (x, y)
+                    | dx <- [-1 .. 1],
+                      let x = r + dx,
+                      dy <- [-1 .. 1],
+                      let y = c + dy,
+                      (x /= r || y /= c)
+                        && x >= 0
+                        && x < length l
+                        && y >= 0
+                        && y < length (head l),
+                      (l !! x) !! y == '*'
+                  ]
+           in if n == 0 then ' ' else head (show n)
+      | c <- [0 .. (length (l !! r) - 1)]
+    ]
+    | r <- [0 .. (length l - 1)]
+  ]
